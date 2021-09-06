aa4c28b1fde32ba878902fb991129753502687f2
Jennifer Maria Manriquez Burrolas <jennifer.manriquez@192.168.3.26>
2021-09-06 18:16:12 -0500

    Can decrease items quantity from shopping-cart-preview

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -7,10 +7,8 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
 import { makeStyles } from "@material-ui/core/styles";
-import { removeFromCart } from '../redux/cart/reducer';
+import { increment, decrement, removeFromCart } from '../redux/cart/reducer';
 import { useDispatch } from 'react-redux';
-import { increment } from "../redux/cart/reducer";
-
 
 
 const useStyles = makeStyles(() => ({
@@ -32,24 +30,7 @@ const useStyles = makeStyles(() => ({
 const ShoppingCart = ({ item }) => {
   const dispatch = useDispatch();
   const [itemTotal, setItemTotal] = useState(0);
-  // const [numItems, setNumItems] = useState(item.quantity);
-  
-  // const [quantity, setQuantity] = useState(item.quantity)
 
-  // const addItem = () => {
-  //   setNumItems(numItems + 1);
-  //   dispatch(increment(item.item_id));
-  // }
-  // const removeItem = (e) => {
-  //   setQuantity(quantity - 1)
-  //   if(quantity <= 0){
-  //     deleteItem()
-  //   }
-  // }
-  // const deleteItem = (e) => {
-  //   //here will be the function to remove item from the storage items
-  //   console.log(`deleted item ${item.name}`)
-  // }
   useEffect(() => {
     let currentTotal = item.quantity * item.price
     setItemTotal(currentTotal)
@@ -68,28 +49,20 @@ const ShoppingCart = ({ item }) => {
             <Typography variant="body1">{item.name}</Typography>
             <Typography variant="body2">Price: {item.price}</Typography>
             </div>
-            
-            <IconButton>
-            {/* <IconButton > */}
+            <IconButton onClick={() => {dispatch(decrement(item.item_id))}}>
               <RemoveIcon />
             </IconButton>
             <Typography variant="body2">{item.quantity}</Typography>
-
-            {/* <IconButton onClick={addItem}> */}
             <IconButton onClick={() => {dispatch(increment(item.item_id))}}>
               <AddIcon />
             </IconButton>
             <Typography variant="body2">Total ${itemTotal}</Typography>
-
-            {/* <IconButton onClick={deleteItem}> */}
             <IconButton onClick={() => {dispatch(removeFromCart(item.item_id))}}>
               <DeleteIcon />
             </IconButton>
           </Box>
           <Divider />
-      </div>
-    
-          
+      </div>    
   );
 };
 
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,15 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import * as actionTypes from './cart-types';
-import { createAction } from "@reduxjs/toolkit";
 import { productsResponse } from '../../dummydata/dummy_products';
-import { current } from "@reduxjs/toolkit";
 
 
 const initialState = {
     //USING DUMMY DATA
     products: productsResponse.data,
     cartItems: [],
-    currentItem: null,
 };
 
 const cartSlice = createSlice({
@@ -39,7 +35,6 @@ const cartSlice = createSlice({
             }      
         },
         increment: (state, action) => {
-            console.log('id is ', action.payload)
             return {
                 ...state,  
                 cartItems: state.cartItems.map((item) => item.item_id === action.payload ? {
@@ -49,37 +44,16 @@ const cartSlice = createSlice({
             }
         },
         decrement: (state, action) => {
-            console.log('id is ', action.payload)
             return {
                 ...state,  
-                cartItems: state.cartItems.map((item) => item.item_id === action.payload ? {
+                cartItems: state.cartItems.map((item) => (item.item_id === action.payload && item.quantity > 1) ? {
                     ...item, quantity: item.quantity - 1} : 
                     item
                 )
             }
         },
-        // adjustQuantity: (state, action) => {
-        //     return {
-        //         ...state,  
-        //         cart: state.cartItems.map((item) => item.item_id !== action.payload.id ? {
-        //             ...item, quantity: action.payload.quantity} : 
-        //             item
-        //         )
-        //     }
-        // },
-        loadCurrentItem: (state, action) => {
-            return {
-                ...state,  
-                currentItem: action.payload
-            }
-        },
     },
 });
 
-// export const addToCart = createAction('ADD_TO_CART');
-// export const removeFromCart = createAction('REMOVE_FROM_CART');
-// export const adjustQuantity = createAction('ADJUST_QUANTITY');
-// export const loadCurrentItem = createAction('LOAD_CURRENT_ITEM');
-
 export default cartSlice.reducer;
-export const { addToCart, removeFromCart, increment } = cartSlice.actions;
+export const { addToCart, removeFromCart, increment, decrement } = cartSlice.actions;
