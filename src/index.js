41b6267357ab4cac72b79c0c06380b4239cf1825
Jennifer Maria Manriquez Burrolas <jennifer.manriquez@192.168.3.26>
2021-09-07 21:06:57 -0500

    Persist among refreshes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
