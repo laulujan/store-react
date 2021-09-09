db05d359604e030f7b08e16ace39d1452d9b2d15
ere <celisacosta.3r3@gmail.com>
2021-09-09 18:13:52 -0500

    create categorycard.tsx

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  root: {
+    width: "13.5em",
+    height: "15em",
+  },
+  mediaContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  mediaContent: {
+    width: "90%",
+    height: "auto",
+  },
+  categoryTitle: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      padding: 0,
+  },
+  categoryLabel : {
+      padding: "0em 0em 2em 0em",
+      fontSize: "1.4em",
+  },
+});
+
+interface Category {
+    id: string;
+    name: string;
+    filename: string;
+    alt: string;
+}
+
+const imagesFolder = "category_icons/";
+
+//export const Header: React.FC<Props> = ({text, color}) => {
+
+const CategoryCard: React.FC<Category> = (props) => {
+    const classes = useStyles();
+    const cardImage = imagesFolder + props.filename;
+    console.log(cardImage);
+    const history = useHistory()
+    const onClick = (categoryName: string) => {
+        history.push(`/category/${categoryName}`)
+    }
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea className={classes.mediaContainer} onClick={()=> onClick(props.name)}>
+                <img className={classes.mediaContent} src={cardImage} alt={props.alt}></img>
+                <Typography 
+                    className={classes.categoryLabel}>
+                    {props.name}
+                </Typography>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default CategoryCard;
