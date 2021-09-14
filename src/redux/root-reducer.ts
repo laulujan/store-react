5d5b9594bf4b601606ff2b7b740a9787d0ab76d1
ere <celisacosta.3r3@gmail.com>
2021-09-13 19:32:47 -0500

    migrate root redux files to TS

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -4,8 +4,9 @@ import { connectRouter } from "connected-react-router";
 import userReducer from "./user/reducer";
 import cartReducer from "./cart/reducer";
 import productsReducer from "./products/reducer"
+import { History } from "history";
 
-export const rootReducer = (history) =>combineReducers({
+export const rootReducer = (history: History<unknown>) =>combineReducers({
   products: productsReducer,
   router: connectRouter(history),
   user: userReducer,
