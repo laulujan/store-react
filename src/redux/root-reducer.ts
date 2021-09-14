19cc0e8c486a1d610beafdea646a6617d82a75d1
Jennifer Manriquez <jennymanb@gmail.com>
2021-09-13 19:37:06 -0500

    root-reducer and root-saga

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,11 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { connectRouter } from "connected-react-router";
+import { History } from "history";
 
 import userReducer from "./user/reducer";
 import cartReducer from "./cart/reducer";
 import productsReducer from "./products/reducer"
 
-export const rootReducer = (history) =>combineReducers({
+export const rootReducer = (history: History) =>combineReducers({
   products: productsReducer,
   router: connectRouter(history),
   user: userReducer,
